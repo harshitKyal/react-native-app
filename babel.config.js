c6fb291b28cf79cc1820b968147b22c4584ac9d6
harshitkyal <harshitkyal@gmail.com>
2020-04-12 00:47:07 +0530

    React components part 2

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    sourceType: "unambiguous",
+    presets: ['babel-preset-expo']
+    
   };
+  
 };
