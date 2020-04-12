67fa7d9723eef5e1eaff52fd0bec01b10b9314e6
harshitkyal <harshitkyal@gmail.com>
2020-04-12 23:32:05 +0530

    using Redux

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import   Main from "./components/MainComponent";
+import Main from "./components/MainComponent";
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
+
+const store = ConfigureStore();
 
 export default class App extends React.Component {
   render(){
     return (
-      // <Text>dsds</Text>
+      <Provider store={store}>
         <Main/>
+      </Provider>
+        
       )
     }
 }
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -7,7 +7,31 @@ import { View ,Text,Platform,Image, StyleSheet,ScrollView} from 'react-native';
 import Dishdetail from './DishdetailComponent';
 import {createStackNavigator,createDrawerNavigator,DrawerItems, SafeAreaView} from 'react-navigation';
 import {Icon} from 'react-native-elements';
+import { connect } from 'react-redux';
+import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 
+// const mapStateToProps = state => {
+//   return {
+
+//   }
+// }
+
+const mapDispatchToProps = dispatch => ({
+    fetchDishes: () => dispatch(fetchDishes()),
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos()),
+    fetchLeaders: () => dispatch(fetchLeaders())
+  })
+  
+  const mapStateToProps = state => {
+    return {
+      dishes: state.dishes,
+      comments: state.comments,
+      promotions: state.promotions,
+      leaders: state.leaders
+    }
+  }
+  
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu ,
     navigationOptions :({navigation})=>({
@@ -173,6 +197,12 @@ const MainNavigator = createDrawerNavigator({
 
 class Main extends Component{
 
+    componentDidMount() {
+        this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
+        this.props.fetchLeaders();
+      }
     render(){
         return(
             <View style={{flex:1}}>
@@ -207,4 +237,4 @@ const styles = StyleSheet.create({
     }
   });
 
-export default Main;
+  export default connect(mapStateToProps, mapDispatchToProps)(Main);
