c7879701ef0c9f64cdf2a0cfaa3d8685ec054725
harshitkyal <harshitkyal@gmail.com>
2020-04-15 13:09:30 +0530

    local notifucation

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Alert,Text, View, ScrollView, StyleSheet, Picker, Switch, Button ,Modal} from 'react-native';
 import DatePicker from 'react-native-datepicker'
 import * as Animatable from 'react-native-animatable';
+import { Notifications } from 'expo';
+import * as Permissions from 'expo-permissions';
+
 
 class Reservation extends Component {
 
@@ -39,7 +42,10 @@ class Reservation extends Component {
             {
                 text: 'OK',
                 // eslint-disable-next-line no-confusing-arrow, no-console
-                onPress: () => this.resetForm(),
+                onPress: () => {
+                    this.presentLocalNotification(this.state.date);
+                    this.resetForm();
+                }
             },
             ],
             { cancelable: false },
@@ -54,6 +60,35 @@ class Reservation extends Component {
             showModal: false
         });
     }
+
+    async obtainNotificationPermission(){
+        let permission  = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+
+        if(permission.status !== 'granted'){
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
+        });
+    }
+    
 
     render (){
         return(
